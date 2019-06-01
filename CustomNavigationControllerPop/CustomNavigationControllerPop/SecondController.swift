ec37886150970b34cede24c4d64cc7ea213f7a3f
ITCenter <410905481@qq.com>
2019-06-01 12:04:39 +0800

    清空第二个控制器下多余的代码

--- CustomNavigationControllerPop/CustomNavigationControllerPop/SecondController.swift
+++ CustomNavigationControllerPop/CustomNavigationControllerPop/SecondController.swift
@@ -12,16 +12,5 @@ class SecondController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let button = UIButton()
-        button.setTitle("点击返回", for: .normal)
-        button.addTarget(self, action: #selector(back), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        let centerConstraint1 = NSLayoutConstraint(item: button, attribute: .centerXWithinMargins, relatedBy: .equal, toItem: view, attribute: .centerXWithinMargins, multiplier: 1, constant: 0)
-        let centerConstraint2 = NSLayoutConstraint(item: button, attribute: .centerYWithinMargins, relatedBy: .equal, toItem: view, attribute: .centerYWithinMargins, multiplier: 1, constant: 0)
-        view.addSubview(button)
-        NSLayoutConstraint.activate([centerConstraint1, centerConstraint2])
-    }
-    @objc func back() {
-        self.navigationController?.popViewController(animated: true)
     }
 }
